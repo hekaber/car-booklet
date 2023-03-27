ea724f5be8fc6a12c74442b32385a16d2d22b72f
Henry Karl Berger <hkberger@gmail.com>
2023-03-27 18:22:59 +0200

    feat:loader for each card

--- app/src/components/MaintenanceCard/MaintenanceCard.tsx
+++ app/src/components/MaintenanceCard/MaintenanceCard.tsx
@@ -1,4 +1,4 @@
-import { Card, CardContent, Typography } from "@mui/material";
+import { Fade, Card, CardContent, CircularProgress, Typography, circularProgressClasses } from "@mui/material";
 import { useEffect, useState } from "react";
 import CarBooklet from "../../classes/contracts/CarBooklet";
 import { IMaintenanceRecord } from "../../classes/interfaces/Interfaces";
@@ -12,32 +12,41 @@ interface IMCProps {
 const MaintenanceCard = (props: IMCProps) => {
 
     const { maintenanceId, carBooklet } = props;
+    const [loading, setLoading] = useState<boolean>(true);
     const [maintenance, setMaintenance] = useState<IMaintenanceRecord | null>(null);
 
     const getMaintenanceData = async () => {
         const data: IMaintenanceRecord | null = carBooklet ? await carBooklet.getMaintenanceRecord(maintenanceId) : null;
-        console.log("Maintenance data", data);
+        setLoading(false);
         setMaintenance(data);
     }
 
     useEffect(() => { getMaintenanceData() }, []);
 
     return (
-        <Card sx={{ minWidth: 275 , marginBottom: 2}}>
-            <CardContent>
-                <Typography variant="h5" component="div">
-                    {maintenance?.mileage} km.
-                </Typography>
-                <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                    <>
-                        Maintenance date: {maintenance?.timestamp ? new Date(maintenance.timestamp * 1000).toDateString() : "Not specified"}
-                    </>
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                    { maintenance?.description }
-                </Typography>
-            </CardContent>
-        </Card>
+        <>
+            <Card sx={{ minWidth: 275, marginBottom: 2 }}>
+                <CardContent>
+                    {loading
+                        ? <CircularProgress />
+                        :
+                        <>
+                            <Typography variant="h5" component="div">
+                                {maintenance?.mileage} km.
+                            </Typography>
+                            <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                                <>
+                                    Maintenance date: {maintenance?.timestamp ? new Date(maintenance.timestamp * 1000).toDateString() : "Not specified"}
+                                </>
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                {maintenance?.description}
+                            </Typography>
+                        </>
+                        }
+                </CardContent>
+            </Card>
+        </>
     );
 }
 
